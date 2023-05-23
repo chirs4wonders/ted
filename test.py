a3f36bbd8d0352820a03f38bbdb9cb1a3f21a470
chris4wonders <chi4wonders@gmail.com>
2023-05-23 17:55:17 +0100

    This is my first commit

---
+++ test.py
@@ -0,0 +1,18 @@
+print('Hello World')
+
+#import numpy as np
+import pandas as pd
+
+
+dict = {'gender':{'John':'m','Alex':'f'},
+        'age':{'John':22,'Alex':34}}
+        
+print(dict)
+
+df1 = pd.DataFrame(dict)
+
+print(df1)
+
+print(' This is beginning to sync')
+
+print('nice one chris')
